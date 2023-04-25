4f747f4cdb4341309438ee5f83e230ea2249aa2f
kajenk <86421060+kajenk@users.noreply.github.com>
2023-04-24 20:19:01 -0400

    terraform

--- server.tf
+++ server.tf
@@ -3,6 +3,7 @@ data "aws_ami" "centos" {
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]  
 }
+##
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -19,3 +20,177 @@ resource "aws_route53_record" "frontend" {
   ttl     = 30
   records = [aws_instance.frontend.private_ip]
 }
+
+#####
+##
+resource "aws_instance" "mongode" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z03680352BDV8JQ0ARSB5"
+  name    = "mongodb-dev.netseclab.ca"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
+#####
+##
+resource "aws_instance" "catalogue" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z03680352BDV8JQ0ARSB5"
+  name    = "catalogue-dev.netseclab.ca"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
+#####
+##
+resource "aws_instance" "radis" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "radis"
+  }
+}
+
+resource "aws_route53_record" "radis" {
+  zone_id = "Z03680352BDV8JQ0ARSB5"
+  name    = "radis-dev.netseclab.ca"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.radis.private_ip]
+}
+
+#####
+##
+resource "aws_instance" "user" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z03680352BDV8JQ0ARSB5"
+  name    = "user-dev.netseclab.ca"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+
+#####
+##
+resource "aws_instance" "cart" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z03680352BDV8JQ0ARSB5"
+  name    = "cart-dev.netseclab.ca"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
+
+#####
+##
+resource "aws_instance" "mysql" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z03680352BDV8JQ0ARSB5"
+  name    = "mysql-dev.netseclab.ca"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
+#####
+##
+resource "aws_instance" "shipping" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z03680352BDV8JQ0ARSB5"
+  name    = "shipping-dev.netseclab.ca"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
+
+#####
+##
+resource "aws_instance" "rabbitmq" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z03680352BDV8JQ0ARSB5"
+  name    = "rabbitmq-dev.netseclab.ca"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
+#####
+##
+resource "aws_instance" "payment" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "payment"
+  }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z03680352BDV8JQ0ARSB5"
+  name    = "payment-dev.netseclab.ca"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
+
+#####
+
